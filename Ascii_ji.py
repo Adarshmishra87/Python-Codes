205fa8ebbe99b0c8ad8f5f6a75e3909988e1df36
Adarsh Mishra <143173802+Adarshmishra87@users.noreply.github.com>
2024-11-22 11:39:30 +0530

    Update Ascii_ji.py

--- Ascii_ji.py
+++ Ascii_ji.py
@@ -1,7 +1,7 @@
-discr = { "Adarsh Mishra": "A guy who have guts to conquer any thing he desired.",
-         "Adarsh": "A person with cool skills and also a moneylianre guy who is next are say new dude into technology",}
+discr = { "Shaingel": "A guy who have guts to conquer any thing he desired.",
+         "Adam": "Shoot the world",}
 
-discr["Adarsh Mishra"] = "Move, communicate,kill."
+discr["Shaingel"] = "Move, communicate,kill."
 # print(discr)
 
 for key in discr:
